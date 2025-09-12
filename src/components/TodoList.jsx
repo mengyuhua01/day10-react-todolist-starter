18ad01a7c70956e1c34874385c27e65519ff3680
LEO MENG <mengle@oocl.com>
2025-09-12 15:31:12 +0800

    refactor: show content form to-list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,20 @@
+import {useContext} from "react";
+import {TodoContext} from "../contexts/TodoContext";
+import './TodoList.css';
+
 const TodoList = () => {
-  return (
-      <div>This is the TodoList Component.</div>
-  );
+    const {state,dispatch} = useContext(TodoContext)
+    // function toggleDone(id){
+    //     const action = {type:"DONE",id:id}
+    //     dispatch(action)
+    // }
+    return (
+        <div>This is the TodoList Component.
+            {
+                state.map(item => <div className={"to-do"}>{item.text}</div>)
+            }
+        </div>
+    );
 }
 
 export default TodoList
