29dc5929de6f0132f48f11867e4ce8b76ece384d
LEO MENG <mengle@oocl.com>
2025-09-16 20:02:00 +0800

    feat: update API base URL and add request/response interceptors for logging and error handling

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,6 +1,6 @@
 import  axios from 'axios'
 const instance = axios.create({
-    baseURL: 'https://68c78d085d8d9f5147322643.mockapi.io/api/'
+    baseURL: 'http://localhost:8080/'
 })
 
 export const getTodos = async () => await instance.get('/todos')
@@ -10,3 +10,38 @@ export const addTodos = async (todo) => await instance.post('/todos',todo)
 export const deleteTodos = async (id) => await instance.delete(`/todos/${id}`)
 
 export const updateTodos = async (id,todo) => await instance.put(`/todos/${id}`,todo)
+
+instance.interceptors.request.use(
+    (config) => {
+        // request configuration
+        console.log("request success", config)
+        config.metadata = {
+            startTime: Date.now()
+        }
+        return config;
+    },
+    (error) => {
+        // handle request error
+        return Promise.reject(error);
+    }
+);
+
+instance.interceptors.response.use(
+    (response) => {
+        // handle response
+        console.log("response success", response)
+        console.log('Api duration is' + (Date.now() - response.config.metadata.startTime) +'ms')
+        return response;
+    },
+    (error) => {
+        // handle response error
+        const {status, data} = error.response;
+        if (status === 401) {
+            alert(`response Error ${status} ${data}`)
+            console.log(error.response)
+            window.location.href = '/login';
+            // do something
+        }
+        return Promise.reject(error);
+    }
+);
